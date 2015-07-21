86308cdd6ebce8155e3c223d2d3fdb50ff54e85b
akhnf <haneefx7@gmail.com>
2015-07-21 20:41:56 +0800

    Added solution to 5th exercise - FILTERED LS

---
+++ f-make-it-modular/f-make-it-modular-filter.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var path = require('path')
+
+module.exports = function (dir, fileType, callback) {
+  fs.readdir(dir, function (err, list) {
+    if (err) {
+      return callback(err)
+    }
+
+    list = list.filter(function(file) {
+      return path.extname(file) === '.'.concat(fileType)
+    })
+
+    callback(null, list)
+  })
+}
