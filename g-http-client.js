cd841151bfa0fe4b2b9aa4f89447c78bef3faa8b
akhnf <haneefx7@gmail.com>
2015-07-22 23:03:15 +0800

    Added solution to 7th exercise - HTTP CLIENT

--- g-http-client.js
+++ g-http-client.js
@@ -3,7 +3,7 @@ var url = process.argv[2]
 
 http.get(url, function (response) {
   response.setEncoding('utf8')
-  response.on('data', function (data) {
-    console.log(data)
+  response.on('data', function (chunk) {
+    console.log(chunk)
   })
 })
